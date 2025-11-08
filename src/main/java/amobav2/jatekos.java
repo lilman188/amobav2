24756bb4403c6b950866465f012b0b54b5c9290e
lilman188 <reskoimre@gmail.com>
2025-11-08 18:23:46 +0100

    jatekos.java has been created

---
+++ src/main/java/amobav2/jatekos.java
@@ -0,0 +1,15 @@
+package amobav2;
+
+public abstract class Player {
+    private final char symbol;
+
+    public Player(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public abstract Move getMove(Board board);
+}
